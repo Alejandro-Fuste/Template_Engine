d306827d689e7ca1c4afb6d17e042c60771887a2
Alejandro F <afuste7@gmail.com>
2020-03-09 17:41:38 -0400

    added engineer class with inheritance from employee, and passed all test for engineer test file

--- develop/lib/Engineer.js
+++ develop/lib/Engineer.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require('../lib/Employee');
+
+class Engineer extends Employee {
+	constructor(name, id, email, GitHUb) {
+		super(name, id, email);
+		this.github = GitHUb;
+	}
+
+	getRole() {
+		return `Engineer`;
+	}
+
+	getGithub() {
+		return this.github;
+	}
+}
+
+const e = new Engineer('Foo', 1, 'test@test.com', 'GitHubUser');
+
+e.getRole();
+e.getGithub();
+
+module.exports = Engineer;
